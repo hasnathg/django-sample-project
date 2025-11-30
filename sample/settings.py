2bd1753d5c885437d8cd6b9ad0de61832a5944fd
Hasnath Karim <hasnath.karim@gmail.com>
2025-11-30 16:46:58 +0000

    initial app created

--- sample/settings.py
+++ sample/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cuisine',
 ]
 
 MIDDLEWARE = [
@@ -119,4 +120,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+MEDIA_URL = '/pics/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'cuisine/pics')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
